26cf8dd20e3b6dcea01254d0df9cd9e4b7f9f127
wangjian <jian.wang@webstudio.com.cn>
2020-06-19 11:27:03 +0800

    二分查找法变形问题

--- src/wjchenge/arithmetic/Bsearch.java
+++ src/wjchenge/arithmetic/Bsearch.java
@@ -2,7 +2,7 @@ package wjchenge.arithmetic;
 
 /**
  * 二分查找算法
- * @author user
+ * @author wjchenge
  */
 public class Bsearch {
 
@@ -18,7 +18,7 @@ public class Bsearch {
         int l = 0;
         int r = a.length;
         while (l <= r) {
-            int mid = l + (r - l) /2;
+            int mid = l + ((r - l) >> 1);
             if (a[mid] == value) {
                 return mid;
             } else if (a[mid] > value) {
@@ -30,8 +30,113 @@ public class Bsearch {
         return -1;
     }
 
+    /**
+     * 假定数组从小到大排序，其中存在重复元素，查找给定的值第一次出现的位置
+     * @param a
+     * @param value
+     * @return
+     */
+    public static int bsearchFirst(int[] a, int value){
+        int result = -1;
+        if (a == null || a.length == 0) return result;
+        int l = 0;
+        int r = a.length;
+        while (l <= r) {
+            int mid = l + ((r - l) >> 1);
+            if (a[mid] == value) {
+                if (mid == 0 || (a[mid - 1]) != value) return mid;
+                r = mid - 1;
+            } else if (a[mid] > value) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+
+    /**
+     * 假定数组从小到大排序，其中存在重复元素，查找给定的值最后一次出现的位置
+     * @param a
+     * @param value
+     * @return
+     */
+    public static int bsearchLast(int[] a, int value){
+        int result = -1;
+        if (a == null || a.length == 0) return result;
+        int l = 0;
+        int r = a.length;
+        while (l <= r) {
+            int mid = l + ((r - l) >> 1);
+            if (a[mid] == value) {
+                if (mid == 0 || (a[mid + 1]) != value) return mid;
+                l = mid + 1;
+            } else if (a[mid] > value) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 假定数组从小到大排序，其中存在重复元素，查找第一个大于等于给定值的元素出现的位置
+     * @param a
+     * @param value
+     * @return
+     */
+    public static int bsearchFirstGE(int[] a, int value){
+        int result = -1;
+        if (a == null || a.length == 0) return result;
+        int l = 0;
+        int r = a.length;
+        while (l <= r) {
+            int mid = l + ((r - l) >> 1);
+            if (a[mid] >= value) {
+                if (mid == 0 || (a[mid - 1]) < value) return mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+
+    /**
+     * 假定数组从小到大排序，其中存在重复元素，查找最后一个小于等于给定值的元素出现的位置
+     * @param a
+     * @param value
+     * @return
+     */
+    public static int bsearchFirstLE(int[] a, int value){
+        int result = -1;
+        if (a == null || a.length == 0) return result;
+        int l = 0;
+        int r = a.length;
+        while (l <= r) {
+            int mid = l + ((r - l) >> 1);
+            if (a[mid] <= value) {
+                if (mid == 0 || (a[mid + 1]) > value) return mid;
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         int[] a = SortUtil.getOrderArray(100);
-        System.out.println(bsearch(a, 99));
+        System.out.println(bsearch(a, 55));
+        for (int i = 55; i < 66; i++) {
+            a[i] = 55;
+        }
+        System.out.println(bsearchFirst(a, 55));
+        System.out.println(bsearchLast(a, 55));
+        System.out.println(bsearchFirstGE(a, 53));
+        System.out.println(bsearchFirstLE(a, 55));
     }
 }
